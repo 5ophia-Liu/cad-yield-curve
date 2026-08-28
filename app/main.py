bd09ded340a0628379b5681f206e9a135da99236
5ophia-Liu <sophia.s.liu@gmail.com>
2026-08-27 20:40:01 -0400

    initial commit

---
+++ app/main.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+
+app = FastAPI(title="CAD Yield Curve API")
+
+
+@app.get("/")
+def health_check():
+    return {"status": "ok", "message": "CAD Yield Curve API is running"}
